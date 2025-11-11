45faa00ff4af5de3aa8e26f6682aa5e2d1247a93
PedroVitorDeMeloDaCcruz <ppmelocruz98@hotmail.com>
2025-11-11 13:19:50 -0300

    Modulo9

---
+++ Modulo9-Exercicios/Tarefa_wrappers/Mod9/src/Coversor.java
@@ -0,0 +1,19 @@
+public class Coversor {
+    public static void main(String[] args) {
+        meuAtletico();
+        
+    }
+    public static void meuAtletico() {
+        System.out.println("O time Atletico tem como seu numero o");
+        int Galo = 13;
+        Long Doido = (long) Galo;
+        
+
+        System.out.println(Doido);
+
+        System.out.println("referencia ao numero do jogo do Bicho");
+        
+        
+    }
+
+}
