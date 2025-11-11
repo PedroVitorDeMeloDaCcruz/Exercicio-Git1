b20a68d3f5137a61e68b3a734cfad7000077fdb4
PedroVitorDeMeloDaCcruz <ppmelocruz98@hotmail.com>
2025-11-11 10:53:25 -0300

    Modulo8

---
+++ Modulo8-Exercicios/Tarefa Calculo Media/Mod8/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) {
+        mediaEscolar();
+        
+    }
+    
+    public static void mediaEscolar(){
+        System.out.println("Media Da Turma");
+        int Aluno1 = 7;
+        int Aluno2 = 5;
+        int Aluno3 = 9;
+        int Aluno4 = 3;
+
+        int Media = ( Aluno1+Aluno2+Aluno3+Aluno4)/4;
+
+        System.out.println(Media);
+    
+    }
+
+}
